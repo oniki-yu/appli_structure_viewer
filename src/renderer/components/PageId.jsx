add36003e65e2fd57379d73c86509f636b59d546
Oniki Yuki <onikiyuki@ip-172-16-1-101.ap-northeast-1.compute.internal>
2017-04-24 15:08:34 +0900

    React-Routerの最小構成完成（更新）

--- src/renderer/components/PageId.jsx
+++ src/renderer/components/PageId.jsx
@@ -5,9 +5,11 @@ import { Link, hashHistory } from "react-router";
 export default class PageId extends React.Component {
     render() {
         console.log(hashHistory);
+        console.log(this.props);
         return (
             <div>
-                <h2>PageId</h2>
+                <h2>pageId</h2>
+                <h2>{this.props.params.pageId}</h2>
                 <div onClick={()=>hashHistory.goBack()}>Go BACK</div>
                 });
             </div>
