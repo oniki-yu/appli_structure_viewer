0c3267df7ee0cc0d66edb4fbf8d0b16c75152b7e
Oniki Yuki <onikiyuki@ip-172-16-1-101.ap-northeast-1.compute.internal>
2017-05-01 20:32:56 +0900

    コードの細かい修正

--- src/renderer/components/ApiData/Author.jsx
+++ src/renderer/components/ApiData/Author.jsx
@@ -7,23 +7,14 @@ export default class Author extends React.Component {
     render() {
         const { data } = this.props;
         const authorIdx = this.props.idx ? this.props.idx : 1;
-        let authors;
-        if (Array.isArray(data)) {
-            authors = data.map((data, key) => {
-                return (
-                    <AuthorData data={data} idx={key} key={key}/>
-                )
-            });
-        } else if (typeof data === 'object') {
-            const authorName = data.name ? "name:  " + data.name : 'no name';
-            authors = <ListItem
-                primaryText={ authorName }
-            />
-        }
+        const authors = Array.isArray(data) ?
+            data.map((data, key) => (<AuthorData data={data} idx={key} key={key}/>)) :
+            (typeof data === 'object') ?
+            (<ListItem primaryText={ data.name ? "name: " + data.name : 'no name'} />) : null;
         return (
             <ListItem
                 key={ authorIdx }
-                primaryText="Autor"
+                primaryText="Author"
                 primaryTogglesNestedList={true}
                 nestedItems={
                     authors
